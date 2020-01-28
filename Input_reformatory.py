f2e8e92a0230ff40a4096c29cee0924c6ebaf955
MrMalfunction <007bohora@gmail.com>
2020-01-29 00:06:41 +0530

    Merge remote-tracking branch 'origin/master'
    Signed-off-by: MrMalfunction <007bohora@gmail.com>
    
    # Conflicts:
    #	Input_reformatory.py

---
+++ Input_reformatory.py
@@ -0,0 +1,11 @@
+file_name = input("Enter the location of file\n")  # this may change
+input_file = open(file_name, "r")
+temp_input = []
+rf = input_file.readline()
+while rf != "":
+    temp_input.append(rf)
+    rf = input_file.readline()
+final_input = []
+for i in temp_input:
+    final_input.append(i.split(" "))
+print(final_input)
