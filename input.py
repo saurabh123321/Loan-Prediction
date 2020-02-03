23301a39c93d3dada3c7321a0207ff108552ce76
saurabh123321 <60282343+saurabh123321@users.noreply.github.com>
2020-02-03 22:24:23 +0530

    Update input.py

--- input.py
+++ input.py
@@ -1,1 +1,13 @@
+file_name = input("Enter the location of file")  # this may change
+input_file = open("train.csv", "r")   # directly added data file in the project folder with address C:\Users\khule\PycharmProjects\Loan-Prediction
+
+temp_input = []
+rf = input_file.read()
+while rf != "":
+    temp_input.append(rf)
+    rf = input_file.readline()
+final_input = []
+for i in temp_input:
+    final_input.append(i.split(","))
+print(final_input)
 
